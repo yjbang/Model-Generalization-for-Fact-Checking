993c7d64506e7feaaab6c77513151ca216de0a5a
yjbang <yjbang@connect.ust.hk>
2020-12-06 14:00:23 +0800

    training script

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -895,10 +895,37 @@ class FakeNewsDataset(Dataset):
     LABEL2INDEX = {'fake': 0, 'real': 1}
     INDEX2LABEL = {0: 'fake', 1: 'real'}
     NUM_LABELS = 2
+    EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "\U000024C2-\U0001F251"
+    "]+")
+
+    def preprocess_tweet(text):
+        text = re.sub(r'(https://\S+)', '<URL>', text)
+        #     text = text.replace('THREAD: ', '')
+        text = self.EMOJI_PATTERN.sub(r'', text)
+        encoded_string = text.encode("ascii", "ignore")
+        text = encoded_string.decode()
+        # text = text.replace('#', '')
+        text = text.replace('&amp;', '&')
+
+        return text
     
     def load_dataset(self, path): 
         df = pd.read_csv(path, sep='\t')
         df['label'] = df['label'].apply(lambda lab: self.LABEL2INDEX[lab])
+        # df['tweet'] = df['tweet'].apply(lambda x: self.preprocess_tweet(x))
         return df
     
     def __init__(self, dataset_path, tokenizer, no_special_token=False, *args, **kwargs):
